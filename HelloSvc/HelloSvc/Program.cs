8ff0eb952db1403f983fdd26362036acc101c316
cammerman <cammerman@gmail.com>
2011-01-22 17:04:03 -0600

    Created empty HelloSvc project

---
+++ HelloSvc/HelloSvc/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloSvc
+{
+    public static class Program
+    {
+			public static void Main(String[] args)
+			{
+			}
+    }
+}
