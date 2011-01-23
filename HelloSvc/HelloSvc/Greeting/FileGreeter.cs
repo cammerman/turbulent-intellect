3a5d7bcb13852be4e7d8c89d99a27a9899ecfb2f
cammerman <cammerman@gmail.com>
2011-01-22 18:49:11 -0600

    Added greeter service. Added IoC.

---
+++ HelloSvc/HelloSvc/Greeting/FileGreeter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace HelloSvc.Greeting
+{
+	internal class FileGreeter : IGreeter
+	{
+		public FileGreeter()
+		{
+			
+		}
+
+		protected virtual String Filename()
+		{
+			return
+				Path.Combine(
+					Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+					"hello.txt");
+		}
+
+		protected virtual FileStream OpenFile()
+		{
+			return
+				new FileStream(
+					Filename(),
+					FileMode.OpenOrCreate,
+					FileAccess.Write,
+					FileShare.Read);
+		}
+
+		public void SayHello()
+		{
+			using (var fs = OpenFile())
+			using (var writer = new StreamWriter(fs))
+			{
+				writer.Write("Hello World!");
+			}
+		}
+	}
+}
--- HelloSvc/HelloSvc/Program.cs
+++ HelloSvc/HelloSvc/Program.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Autofac;
+
 namespace HelloSvc
 {
     public static class Program
     {
 			public static void Main(String[] args)
 			{
+				var iocBootstrap = new IocBootstrapper();
+				var container = iocBootstrap.Build();
+
+				var greeter = container.Resolve<Greeting.IGreeter>();
+
+				greeter.SayHello();
 			}
     }
 }
